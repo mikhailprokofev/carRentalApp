5e1661a110dfc29648bcb7e32dcfdb2ed92d1b32
user <user@users-Mac-mini.local>
2022-12-20 12:43:37 +0300

    feat: add migration for Car and Rental;

---
+++ rental_app/database/migrations/2022_12_19_142328_create_cars_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('number_plate');
+            $table->string('color');
+            $table->string('type');
+            $table->string('description');
+            $table->integer('base_salary');
+            $table->string('model');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cars');
+    }
+};
