3761c10a0a309b4be03e95032d6e32af70439b47
user <user@users-Mac-mini.local>
2022-12-20 12:44:08 +0300

    feat: add entity of Car --all and Rental --all;

---
+++ rental_app/app/Models/Car.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Rental;
+
+class Car extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id',
+        'number_plate',
+        'color',
+        'type',
+        'description',
+        'base_salary',
+        'model',
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * Атрибуты, которые должны быть типизированы.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'options' => 'array',
+    ];
+
+    /**
+     * Атрибуты, которые должны быть типизированы.
+     *
+     * @var array
+     */
+    public function rental()
+    {
+        return $this->hasMany(Rental::class);
+    }
+}
---
+++ rental_app/app/Models/Rental.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Car;
+
+class Rental extends Model
+{
+        use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id',
+        'start_salary',
+        'rental_start',
+        'rental_end',
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * Атрибуты, которые должны быть типизированы.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'options' => 'array',
+    ];
+
+    /**
+     * Атрибуты, которые должны быть типизированы.
+     *
+     * @var array
+     */
+    public function rental()
+    {
+        return $this->belongsTo(Car::class);
+    }
+}
