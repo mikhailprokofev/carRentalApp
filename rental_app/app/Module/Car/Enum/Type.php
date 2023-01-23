935ce2d5ca3ed319d5ac0a237d3f8e2b8b7f283a
Anastasiya <anastasia.shamsudinova@fojin.tech>
2023-01-23 11:43:56 +0300

    Вычисление базовой суммы по равному интервалу

--- rental_app/app/Module/Car/Enum/Type.php
+++ rental_app/app/Module/Car/Enum/Type.php
@@ -14,12 +14,52 @@ enum Type: string
     case Business = 'business';
     case Luxury = 'luxury';
 
-    public function cost(): array|int
+    private function cost(): ?int
     {
         return match($this) {
             self::Econom => 1000,
-            self::Business => ['from' => 1001, 'to' => 4999],
             self::Luxury => 5000,
+            default => 0,
         };
+    }
+
+    private function priority(): int
+    {
+        return match($this) {
+            self::Business => 1,
+            self::Luxury => 2,
+            default => 0,
+        };
+    }
+
+    public static function maxCost(): int
+    {
+        return array_reduce(self::cases(), fn (int $max, self $case) => max($case->cost(), $max), 0);
+    }
+
+    public static function minCost(): int
+    {
+        return array_reduce(
+            self::cases(),
+            fn (int $min, self $case) => $case->cost() > 0 ? min($case->cost(), $min) : $min,
+            PHP_INT_MAX,
+        );
+    }
+
+    public function calculateCost(): int
+    {
+        return $this->cost() > 0 ? $this->cost() : $this->calculateCostByPriority();
+    }
+
+    public function calculateCostByPriority(): int
+    {
+        $max = self::maxCost();
+        $min = self::minCost();
+
+        $countChunks = (count(self::cases()) - 2) * 2;
+
+        $costChunk = (int) round(($max - $min) / $countChunks);
+
+        return $min + $costChunk * $this->priority();
     }
 }
